6b9fbcac7816c6e31c99a80034aef21474dc8ecd
GhostTomX <r04522839@ntu.edu.tw>
2017-03-11 09:58:41 +0800

    Navbar 修理终了
    下一步是将原本的Ajax+php查询，修改为不使用Ajax

---
+++ js/aboutus.js
@@ -0,0 +1,8 @@
+    $(document).ready(function () {
+        $('.dropdown').hover(function () {
+            $(this).find('.dropdown-menu').stop(true, true).fadeIn(500);
+        }, function () {
+            $(this).find('.dropdown-menu').stop(true, true).fadeOut(500);
+        });
+       
+    });
